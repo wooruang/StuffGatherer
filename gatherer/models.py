522f8574126a95dc77787082c0c6ceaa1cf703da
wooruang <wooruang@gmail.com>
2020-02-16 19:02:21 +0900

    Add moddels for a structure of gather's data

--- gatherer/models.py
+++ gatherer/models.py
@@ -4,9 +4,20 @@ from django.db import models
 # Create your models here.
 class Data(models.Model):
     title = models.CharField(max_length=200)
-    description = models.TextField()
+    category = models.CharField(max_length=200)
+    sub_category = models.CharField(max_length=200, null=True)
+    origin = models.TextField()
     content = models.BinaryField()
 
     def __str__(self):
         """A string representation of the model."""
         return self.title
+
+
+class Status(models.Model):
+    data = models.IntegerField()
+    status = models.CharField(max_length=50)
+
+    def __str__(self):
+        """A string representation of the model."""
+        return self.data
