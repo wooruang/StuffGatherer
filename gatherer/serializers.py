dd1e3d4d1bfa90ac845eb4a5d4e86879b820e88e
wooruang <wooruang@gmail.com>
2020-02-17 08:38:53 +0900

    Add a serializer and apis for 'Status'

--- gatherer/serializers.py
+++ gatherer/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import Data
+from .models import Data, Status
+
 
 class DataSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,3 +11,13 @@ class DataSerializer(serializers.ModelSerializer):
             'content',
         )
         model = Data
+
+
+class StatusSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = (
+            'id',
+            'data',
+            'status'
+        )
+        model = Status
--- gatherer/views.py
+++ gatherer/views.py
@@ -1,15 +1,25 @@
 from django.shortcuts import render
 from rest_framework import generics
 
-from .models import Data
-from .serializers import DataSerializer
+from .models import Data, Status
+from .serializers import DataSerializer, StatusSerializer
 
 
-class ListPost(generics.ListCreateAPIView):
+class ListData(generics.ListCreateAPIView):
     queryset = Data.objects.all()
     serializer_class = DataSerializer
 
 
-class DetailPost(generics.RetrieveUpdateDestroyAPIView):
+class DetailData(generics.RetrieveUpdateDestroyAPIView):
     queryset = Data.objects.all()
     serializer_class = DataSerializer
+
+
+class ListStatus(generics.ListCreateAPIView):
+    queryset = Status.objects.all()
+    serializer_class = StatusSerializer
+
+
+class DetailStatus(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Status.objects.all()
+    serializer_class = StatusSerializer
