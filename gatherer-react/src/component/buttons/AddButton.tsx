7c7c625f9e952bba8c4c707f313105f6b2be9d9e
wooruang <wooruang@gmail.com>
2020-04-24 08:53:23 +0900

    Add a dependencies for typescript and rxjs

--- gatherer-react/src/component/buttons/AddButton.tsx
+++ gatherer-react/src/component/buttons/AddButton.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import Icon from '@material-ui/core/Icon';
 
-export default function AddButton(props) {
+interface AddButtonProps {
+    onClick: (event: React.MouseEvent<HTMLButtonElement>) => void
+}
 
+const AddButton: React.SFC<AddButtonProps> = (props) => {
   return (
     <Button onClick={props.onClick} ><Icon color="primary">add_circle</Icon></Button>
   );
 }
+
+export default AddButton;
