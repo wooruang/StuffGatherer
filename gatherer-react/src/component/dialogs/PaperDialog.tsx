1fa06f6bf53831a74dc1039975b577a7d2071239
wooruang <wooruang@gmail.com>
2020-10-23 18:54:59 +0900

    Add a Dialog for Paper

---
+++ gatherer-react/src/component/dialogs/PaperDialog.tsx
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react';
+// import Button from '@material-ui/core/Button';
+// import TextField from '@material-ui/core/TextField';
+// import Dialog from '@material-ui/core/Dialog';
+// import DialogActions from '@material-ui/core/DialogActions';
+// import DialogContent from '@material-ui/core/DialogContent';
+// import DialogContentText from '@material-ui/core/DialogContentText';
+// import DialogTitle from '@material-ui/core/DialogTitle';
+// import Paper from '../Paper/Paper'
+
+
+
+//  interface ImgDialogProps {
+//    open: boolean,
+//    source: string,
+//    fileName: string,
+//    handleClose: () => void
+//  }
+
+// const ImgDialog: React.FC<ImgDialogProps> = (props) => {
+
+//   // // var paperCtx = new paper.PaperScope();
+//   // // var rester = new paper.Raster(props.source);
+//   // const [mounted, setMounted ] = useState(false)
+  
+//   // var _request = null
+
+//   // useEffect(() => { 
+//   //   function resizePaper() {
+//   //     this.forceUpdate()
+//   //     this._request = null
+//   //   }
+
+//   //   function resizeWindow() {
+//   //     if (!this._request) {
+//   //       this._request = requestAnimationFrame(this.resizePaper)
+//   //     }
+//   //   }
+//   //   setMounted(true);
+//   //   window.addEventListener('resize', this.resizeWindow);
+//   // });
+
+
+//   // var activeTool = 'pen';
+//   // var width = 500;
+//   // var height = 500;
+
+//   return (
+//       <Dialog open={props.open} onClose={props.handleClose} aria-labelledby="form-dialog-title">
+//         <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+//         <DialogContent>
+//           <img src={props.source} alt={props.fileName}/>
+//         </DialogContent>
+//       </Dialog>
+//   );
+// }
+
+// export default ImgDialog;
